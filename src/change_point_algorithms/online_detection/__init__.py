3bbe50f2fb586330c4705c51635a4b319d434743
Zhymir Thompson <dzhymir@gmail.com>
2025-08-25 18:08:42 -0400

    Added init for online_detection submodule.

---
+++ src/change_point_algorithms/online_detection/__init__.py
@@ -0,0 +1,10 @@
+from change_point_algorithms._change_point_algorithms import (
+    BocpdModel, EmModel, EmLikelihoodCheck, CusumV0, CusumV1,
+    build_em_model, build_em_early_stop_model
+)
+
+# from change_point_algorithms import online_detection
+#
+# __doc__ = online_detection.__doc__
+# if hasattr(online_detection, "__all__"):
+#     __all__ = online_detection.__all__
