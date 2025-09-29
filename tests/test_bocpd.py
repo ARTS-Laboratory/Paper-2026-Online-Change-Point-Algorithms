a3db25a8b72571543c9d65bb456216c3ed1e5979
Zhymir Thompson <dzhymir@gmail.com>
2025-09-29 05:37:24 -0400

    Modified tests for bocpd.

--- tests/test_bocpd.py
+++ tests/test_bocpd.py
@@ -1,6 +1,7 @@
 import numpy as np
+from change_point_algorithms import _change_point_algorithms
 
-from change_point_algorithms.online_detection.bocpd import bocpd_rust_hybrid
+from change_point_algorithms.online_detection.bocpd import bocpd_rust_hybrid, bocpd_generator
 
 
 def get_parameters():
@@ -22,7 +23,8 @@ def generate_normal_points(mean: float, stddev: float, num_points: int):
 
 class TestRustModels:
     alpha, beta, mu, kappa, lamb = get_parameters()
-    num_unknowns = 100
+    num_unknowns = 200
+    num_to_pass = round(0.95 * num_unknowns)
     safe_mean = mu
     safe_stddev = 1.0
     # safe_stddev: float = (beta * (kappa + 1.0)) / (kappa * alpha)
@@ -36,9 +38,7 @@ class TestRustModels:
         model_gen = bocpd_rust_hybrid(
             my_unknowns, self.mu, self.kappa, self.alpha, self.beta, self.lamb)
         predictions = [item for item in model_gen]
-        # print(predictions)
-        assert all([item == False for item in
-                    predictions]), f'Model predicted that {[item for item in predictions].count(True)} were change points.'
+        assert predictions.count(False) >= self.num_to_pass, f'Model predicted that {predictions.count(True)} were change points.'
 
     def test_bocpd_rust_hybrid_all_abnormal(self):
         my_unknowns = generate_normal_points(
@@ -46,6 +46,49 @@ class TestRustModels:
         model_gen = bocpd_rust_hybrid(
             my_unknowns, self.mu, self.kappa, self.alpha, self.beta, self.lamb)
         predictions = [item for item in model_gen]
-        # print(predictions)
-        assert all([item == True for item in
-                    predictions]), f'Model predicted that {[item for item in predictions].count(False)} were change points.'
+        assert predictions.count(True) >= self.num_to_pass, f'Model predicted that {predictions.count(False)} were change points.'
+
+    def test_bocpd_rust_hybrid_all_normal_probabilities(self):
+        last_prob = 1.0
+        num_unknowns = 100_000
+        prob_threshold = 0.05
+        my_unknowns = generate_normal_points(
+            self.safe_mean, self.safe_stddev, num_unknowns)
+        model = _change_point_algorithms.BocpdModel(self.alpha, self.beta, self.mu, self.kappa, True, threshold=1e-8)
+        predictions = []
+        for idx, event in enumerate(my_unknowns):
+            model.update(event, 1000.0)
+            probability: float = model.predict(event)
+            probability *= last_prob
+            assert probability <= 1.0, f'Probability at idx {idx}: {probability}'
+            is_attack = probability <= prob_threshold
+            last_prob = probability if not is_attack else 1.0
+            predictions.append(is_attack)
+        assert predictions.count(False) >= self.num_to_pass, f'Model predicted that {predictions.count(True)} were change points.'
+
+
+class TestPythonModels:
+    alpha, beta, mu, kappa, lamb = get_parameters()
+    num_unknowns = 200
+    num_to_pass = round(0.95 * num_unknowns)
+    safe_mean = mu
+    safe_stddev = 1.0
+    # safe_stddev: float = (beta * (kappa + 1.0)) / (kappa * alpha)
+    unsafe_mean = 100.0
+    unsafe_stddev = 1.0
+
+    def test_bocpd_python_all_normal(self):
+        my_unknowns = generate_normal_points(
+            self.safe_mean, self.safe_stddev, self.num_unknowns)
+        model_gen = bocpd_generator(
+            my_unknowns, self.mu, self.kappa, self.alpha, self.beta, self.lamb)
+        predictions = [item for item in model_gen]
+        assert predictions.count(False) >= self.num_to_pass, f'Model predicted that {predictions.count(True)} were change points.'
+
+    def test_bocpd_python_all_abnormal(self):
+        my_unknowns = generate_normal_points(
+            self.unsafe_mean, self.unsafe_stddev, self.num_unknowns)
+        model_gen = bocpd_generator(
+            my_unknowns, self.mu, self.kappa, self.alpha, self.beta, self.lamb)
+        predictions = [item for item in model_gen]
+        assert predictions.count(True) >= self.num_to_pass, f'Model predicted that {predictions.count(False)} were change points.'
