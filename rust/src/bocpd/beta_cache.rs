eba498e286e2f5e8aa36840a2fc0cfbea02c8a4d
Zhymir Thompson <dzhymir@gmail.com>
2025-09-30 13:51:15 -0400

    Made new implementation attempt for caching. It was slower than current implementation.

--- rust/src/bocpd/beta_cache.rs
+++ rust/src/bocpd/beta_cache.rs
@@ -26,24 +26,6 @@ impl BetaCache {
 
     pub fn get_value(&mut self, value: f64) -> f64 {
         let key = value.to_bits();
-        // if self.cache.contains_key(&key) {
-        //     let value = self
-        //         .cache
-        //         .get(&key)
-        //         .expect("Cache should always have key here since we checked just before.");
-        //     *value
-        // } else {
-        //     // base case
-        //     let fixed = self.fixed_value;
-        //     let res = match value {
-        //         ..0.0 => todo!("does not expect negative numbers. What should we do in this case?"), // beta(steady_x, increase_y), // technically, should throw error
-        //         0.0..=1.0 => beta(fixed, value),
-        //         1.0.. => (value / (fixed + value)) * beta(fixed, value - 1.0),
-        //         _ => todo!("add functionality for other float types."),
-        //     };
-        //     self.cache.insert(key, res.clone());
-        //     res
-        // }
         // New attempt
         if let std::collections::hash_map::Entry::Vacant(e) = self.cache.entry(key) {
             let fixed = self.fixed_value;
@@ -59,5 +41,26 @@ impl BetaCache {
             let &value = self.cache.get(&key).expect("Cache should always have key here since we checked just before.");
             value
         }
+        // // New attempt 1
+        // if let std::collections::hash_map::Entry::Occupied(mut e) = self.cache.entry(key) {
+        //     let &value = e.get();
+        //     value
+        // } else if let std::collections::hash_map::Entry::Occupied(mut e) = self.cache.entry((value - 1.0).to_bits()) {
+        //     let &beta_val = e.get();
+        //     let fixed = self.fixed_value;
+        //     let res = (value / (fixed + value)) * beta_val;
+        //     e.insert(res);
+        //     res
+        // } else {
+        //     let fixed = self.fixed_value;
+        //     let res = match value {
+        //         ..0.0 => todo!("does not expect negative numbers. What should we do in this case?"), // beta(steady_x, increase_y), // technically, should throw error
+        //         0.0..=1.0 => beta(fixed, value),
+        //         1.0.. => (value / (fixed + value)) * beta(fixed, value - 1.0),
+        //         _ => todo!("add functionality for other float types."),
+        //     };
+        //     self.cache.insert(res.to_bits(), res);
+        //     res
+        // }
     }
 }
