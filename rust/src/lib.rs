7fb212d4649964bf4f6d750e6f19029ba393a6a6
Zhymir Thompson <dzhymir@gmail.com>
2025-09-30 13:53:17 -0400

    Commented out functions and imports not expected to be used.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,12 +1,12 @@
 use std::iter::{once, zip};
-use bocpd::beta_cache::BetaCache;
+// use bocpd::beta_cache::BetaCache;
 use bocpd::bocpd_model::BocpdModel;
-use bocpd::dist_params::DistParams;
-use bocpd::sparse_probs::{SparseProb, SparseProbs};
+// use bocpd::dist_params::DistParams;
+// use bocpd::sparse_probs::{SparseProb, SparseProbs};
 use cusum::{CusumV0, CusumV1};
 use expect_max::em_early_stop_model::EmLikelihoodCheck;
 use expect_max::em_model::EmModel;
-use expect_max::em_model_builder::EmBuilder;
+// use expect_max::em_model_builder::EmBuilder;
 use expect_max::em_model_builder::EmBuilderOne;
 
 use pyo3::prelude::*;
@@ -16,63 +16,63 @@ pub mod bocpd;
 pub mod cusum;
 pub mod expect_max;
 
-/// Updates the probability distribution for a set of T-distributions with observed point.
-#[pyfunction]
-fn calc_probabilities(
-    point: f64,
-    lamb: f64,
-    params: &DistParams,
-    probs: &mut SparseProbs,
-) -> PyResult<()> {
-    let hazard = lamb.recip();
-    let priors = params.priors(point);
-    probs.update_probs(priors, hazard)?;
-    probs.normalize();
-    Ok(())
-}
-
-/// Updates the probability distribution for a set of T-distributions with
-/// observed point and cache for beta.
-#[pyfunction]
-fn calc_probabilities_cached(
-    point: f64,
-    lamb: f64,
-    params: &DistParams,
-    probs: &mut SparseProbs,
-    cache: &mut BetaCache,
-) -> PyResult<()> {
-    let hazard = lamb.recip();
-    let priors = params.priors_cached(point, cache);
-    probs.update_probs(priors, hazard)?;
-    probs.normalize();
-    Ok(())
-}
-
-/// Truncate vectors
-#[pyfunction]
-fn truncate_vectors(
-    threshold: f64,
-    params: &mut DistParams,
-    probs: &mut SparseProbs,
-) -> usize {
-    let threshold_filter: Vec<bool> = probs
-        .iter()
-        .map(|prob| prob.get_value() >= threshold)
-        .collect();
-
-    let mut tf_iter = threshold_filter.iter();
-    params.retain_mut(|_| *tf_iter.next().unwrap());
-    let mut tf_iter = threshold_filter.into_iter();
-    probs.retain_mut(|_| tf_iter.next().unwrap());
-    params.len()
-}
-
-#[pyfunction]
-fn get_change_prob(priors: Vec<f64>, probs: &SparseProbs) -> f64 {
-    zip(priors.iter(), probs.iter())
-        .map(|(change, prob)| change * prob.get_value())
-        .sum()
-}
+// /// Updates the probability distribution for a set of T-distributions with observed point.
+// #[pyfunction]
+// fn calc_probabilities(
+//     point: f64,
+//     lamb: f64,
+//     params: &DistParams,
+//     probs: &mut SparseProbs,
+// ) -> PyResult<()> {
+//     let hazard = lamb.recip();
+//     let priors = params.priors(point);
+//     probs.update_probs(priors, hazard)?;
+//     probs.normalize();
+//     Ok(())
+// }
+//
+// /// Updates the probability distribution for a set of T-distributions with
+// /// observed point and cache for beta.
+// #[pyfunction]
+// fn calc_probabilities_cached(
+//     point: f64,
+//     lamb: f64,
+//     params: &DistParams,
+//     probs: &mut SparseProbs,
+//     cache: &mut BetaCache,
+// ) -> PyResult<()> {
+//     let hazard = lamb.recip();
+//     let priors = params.priors_cached(point, cache);
+//     probs.update_probs(priors, hazard)?;
+//     probs.normalize();
+//     Ok(())
+// }
+//
+// /// Truncate vectors
+// #[pyfunction]
+// fn truncate_vectors(
+//     threshold: f64,
+//     params: &mut DistParams,
+//     probs: &mut SparseProbs,
+// ) -> usize {
+//     let threshold_filter: Vec<bool> = probs
+//         .iter()
+//         .map(|prob| prob.get_value() >= threshold)
+//         .collect();
+//
+//     let mut tf_iter = threshold_filter.iter();
+//     params.retain_mut(|_| *tf_iter.next().unwrap());
+//     let mut tf_iter = threshold_filter.into_iter();
+//     probs.retain_mut(|_| tf_iter.next().unwrap());
+//     params.len()
+// }
+//
+// #[pyfunction]
+// fn get_change_prob(priors: Vec<f64>, probs: &SparseProbs) -> f64 {
+//     zip(priors.iter(), probs.iter())
+//         .map(|(change, prob)| change * prob.get_value())
+//         .sum()
+// }
 
 /// Use builder to construct expectation maximization model.
 #[pyfunction]
@@ -86,21 +86,29 @@ fn build_em_model(
     let normal_iter = once(&normal);
     let param_iter = normal_iter.chain(abnormals.iter());
     let samples: Vec<f64> = zip(param_iter, arr_sizes.iter())
-        .map(|(params, &size)| {
+        .flat_map(|(params, &size)| {
             let (mean, stddev, _probs) = params;
             let rng = rand::rng();
             let n = rand_distr::Normal::new(*mean, *stddev).expect("please don't panic");
             n.sample_iter(rng).take(size as usize)
         })
-        .flatten()
         .collect();
-    let mut em_builder = EmBuilder::new();
-    em_builder
-        .build_normal(mean, stddev, prob)?
+    // let mut em_builder = EmBuilder::new();
+    // em_builder
+    //     .build_normal(mean, stddev, prob)?
+    //     .build_abnormal_from_tuples(&abnormals)?
+    //     .build_epochs(epochs)?
+    //     .build_samples_from_slice(&samples);
+    // Ok(em_builder.get_model())
+    let mut em_builder = EmBuilderOne::new();
+    let final_builder = em_builder.build_normal(mean, stddev, prob)?
         .build_abnormal_from_tuples(&abnormals)?
         .build_epochs(epochs)?
-        .build_samples_from_slice(&samples);
-    Ok(em_builder.get_model())
+        .build_samples_from_slice(&samples)
+        .next_builder()?
+        .build_likelihoods()
+        .next_builder()?;
+    Ok(final_builder.get_standard_model())
 }
 
 #[pyfunction]
@@ -114,13 +122,12 @@ fn build_em_early_stop_model(
     let normal_iter = once(&normal);
     let param_iter = normal_iter.chain(abnormals.iter());
     let samples: Vec<f64> = zip(param_iter, arr_sizes.iter())
-        .map(|(params, &size)| {
+        .flat_map(|(params, &size)| {
             let (mean, stddev, _probs) = params;
             let rng = rand::rng();
             let n = rand_distr::Normal::new(*mean, *stddev).expect("please don't panic");
             n.sample_iter(rng).take(size as usize)
         })
-        .flatten()
         .collect();
     let mut em_builder = EmBuilderOne::new();
     let mut final_builder = em_builder.build_normal(mean, stddev, prob)?
@@ -139,16 +146,8 @@ fn build_em_early_stop_model(
 #[pymodule]
 #[pyo3(name = "_change_point_algorithms")]
 fn change_point_algorithms(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(calc_probabilities_cached, m)?)?;
-    m.add_function(wrap_pyfunction!(calc_probabilities, m)?)?;
-    m.add_function(wrap_pyfunction!(truncate_vectors, m)?)?;
-    m.add_function(wrap_pyfunction!(get_change_prob, m)?)?;
     m.add_function(wrap_pyfunction!(build_em_model, m)?)?;
     m.add_function(wrap_pyfunction!(build_em_early_stop_model, m)?)?;
-    m.add_class::<BetaCache>()?;
-    m.add_class::<DistParams>()?;
-    m.add_class::<SparseProb>()?;
-    m.add_class::<SparseProbs>()?;
     m.add_class::<BocpdModel>()?;
     m.add_class::<EmModel>()?;
     m.add_class::<EmLikelihoodCheck>()?;
