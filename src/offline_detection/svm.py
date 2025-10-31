0374ce7c97ea95877f4ef3c7264b32b73073cd23
Zhymir Thompson <dzhymir@gmail.com>
2025-10-30 23:17:32 -0400

    Refactored import statements, added partial docstring.

--- src/offline_detection/svm.py
+++ src/offline_detection/svm.py
@@ -1,14 +1,17 @@
 from collections.abc import Sequence
 
 import numpy as np
+from offline_detection.offline_model_helpers import (
+    dense_to_time_intervals, estimator_prediction,
+    format_single_feature_data)
 from sklearn.svm import SVC
-
-from offline_detection.offline_model_helpers import dense_to_time_intervals, estimator_prediction, \
-    format_single_feature_data
 
 
 def get_svm_model(train_data: np.ndarray, train_labels: np.ndarray, time_vec: Sequence[float], data: np.ndarray) -> tuple[list, list]:
-    """ """
+    """ Return predictions of normal and abnormal from svm model.
+
+        :returns: Tuple of normal and abnormal intervals.
+    """
     model = SVC()
     reshaped_training, reshaped_data = format_single_feature_data(train_data, data)
     predictions = estimator_prediction(
@@ -16,6 +19,3 @@ def get_svm_model(train_data: np.ndarray, train_labels: np.ndarray, time_vec: Se
     # 1.0 is normal, -1.0 is abnormal
     out = (prediction != 1.0 for prediction in predictions)
     return dense_to_time_intervals(time_vec, out)
-    # shocks, non_shocks = dense_to_time_intervals(time_vec, out)
-    # return shocks, non_shocks
-
