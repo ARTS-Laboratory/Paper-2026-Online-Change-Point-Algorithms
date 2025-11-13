5b3393a81269bbdbc2a5547dd086f360af8a38a4
Zhymir Thompson <dzhymir@gmail.com>
2025-11-13 17:31:38 -0500

    Modified to only send warning if trying to use progress without dependency.

--- src/change_point_algorithms/online_detection/model_helpers.py
+++ src/change_point_algorithms/online_detection/model_helpers.py
@@ -3,10 +3,7 @@ import warnings
 try:
     from tqdm import tqdm
 except ModuleNotFoundError:
-    warnings.warn('tqdm module not included in environment.')
-except ImportError:
-    warnings.warn('Expected tqdm to be installed, but cannot find it.')
-    tqdm = None
+    ...
 
 
 def detection_to_intervals_for_generator_v1(
@@ -41,5 +38,5 @@ def detection_to_intervals_for_generator_v1_with_progress(
         my_generator = tqdm(model_generator, total=iter_len)
         return detection_to_intervals_for_generator_v1(time_vec, begin, my_generator, start_offset)
     except NameError:
-        warnings.warn('tqdm module not included in environment.\ndefaulting to run without progress.')
+        warnings.warn('tqdm module not found in environment. Running without progress bars.')
         return detection_to_intervals_for_generator_v1(time_vec, begin, model_generator, start_offset)
