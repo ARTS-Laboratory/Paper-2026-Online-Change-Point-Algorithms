6a07bbb540b58c92b8dadee6ed799fd24e8693ac
ZHYMIR <dzhymir@gmail.com>
2025-11-17 17:02:32 -0500

    Commented out to prevent crashes.

--- src/change_point_algorithms/__init__.py
+++ src/change_point_algorithms/__init__.py
@@ -4,12 +4,12 @@
 # if hasattr(change_point_algorithms, "__all__"):
 #     __all__ = change_point_algorithms.__all__
 
-from change_point_algorithms import _change_point_algorithms
-__doc__ = _change_point_algorithms.__doc__
-if hasattr(_change_point_algorithms, "__all__"):
-    __all__ = _change_point_algorithms.__all__
-
-from change_point_algorithms._change_point_algorithms import (
-    BocpdModel, EmModel, EmLikelihoodCheck, CusumV0, CusumV1,
-    build_em_model, build_em_early_stop_model
-)
+# from change_point_algorithms import _change_point_algorithms
+# __doc__ = _change_point_algorithms.__doc__
+# if hasattr(_change_point_algorithms, "__all__"):
+#     __all__ = _change_point_algorithms.__all__
+#
+# from change_point_algorithms._change_point_algorithms import (
+#     BocpdModel, EmModel, EmLikelihoodCheck, CusumV0, CusumV1,
+#     build_em_model, build_em_early_stop_model
+# )
