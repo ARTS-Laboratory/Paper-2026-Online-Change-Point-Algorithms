378de1ad0c7ffe331e5e96c8fb31034753d98aa1
ZHYMIR <dzhymir@gmail.com>
2025-11-17 17:20:17 -0500

    Added docstring.

--- src/change_point_algorithms/memory_profiling/generate_data.py
+++ src/change_point_algorithms/memory_profiling/generate_data.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 def generate_normal_data(mean: float, std_dev: float, num_samples: int):
-    """ """
+    """ Generate vector of normal data."""
     rng = np.random.default_rng()
     return rng.normal(mean, std_dev, size=num_samples)
