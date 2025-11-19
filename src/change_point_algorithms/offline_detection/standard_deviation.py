0f5eb4de90ab237613f5ea817a224e3356ae38de
Zhymir Thompson <dzhymir@gmail.com>
2025-11-19 00:06:38 -0500

    removed function.

--- src/change_point_algorithms/offline_detection/standard_deviation.py
+++ src/change_point_algorithms/offline_detection/standard_deviation.py
@@ -56,11 +56,3 @@ def get_stdev_intervals(
         else:
             non_shock_intervals.append((time[low], time[high - 1]))
     return shocks, non_shocks, devs
-
-
-def get_plot_std_dev(file_path):
-    times, data = read_data_from_file(file_path)
-    num_samples = 500000  # Temporary magic number for data length
-    shock_intervals, non_shock_intervals, deviation = get_stdev_intervals_v2(times, data, num_samples)
-    fig = plot_shock(times, data, shock_intervals, non_shock_intervals)
-    return shock_intervals, non_shock_intervals, fig
